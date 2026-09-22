07d92a62c7bd0c49608512be83b38e89dee962de
H4K386 <nkhanhhuy10062006@gmail.com>
2026-09-22 14:26:14 +0700

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _0306241284_NguyenKhanhHuy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
